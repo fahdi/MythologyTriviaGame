0e27ae1a046a81109aa76c8cc8771c46ad0863d0
Imran khan <imranloveh01@gmail.com>
2024-05-27 02:52:34 +0500

    chore: Update leaderboard.js to handle empty leaderboard data and display appropriate message

--- scripts/leaderboard.js
+++ scripts/leaderboard.js
@@ -8,15 +8,21 @@ async function getLeaderboard() {
 
         const response = await fetch('http://localhost:5000/leaderboard')
         const data = await response.json();
-        console.log(data);
         leaderboardData = data;
         rederData();
     } catch (error) {
         console.error(error);
+        list.innerHTML = '<li style="color:tomato; font-size:.9rem;">Something went wrong. Check your Network! </li>';
     }
 }
 
 function rederData() {
+
+    if (leaderboardData.length === 0 || !leaderboardData) {
+        list.innerHTML = '<li>No data</li>';
+        return;
+    }
+
     leaderboardData.forEach((data, index) => {
         const li = document.createElement('li');
         li.innerHTML = `
@@ -25,6 +31,7 @@ function rederData() {
             <span>${data.score}</span>
         `;
         list.appendChild(li);
+
     });
 }
 
