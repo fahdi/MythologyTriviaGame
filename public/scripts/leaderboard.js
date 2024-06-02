fceb78bea20b4cc8136b8898d007a7c139b9f0f5
isupercoder <info@fahdmurtaza.com>
2024-06-03 04:41:36 +0500

    Get leaderboard up and running

--- public/scripts/leaderboard.js
+++ public/scripts/leaderboard.js
@@ -1,23 +1,19 @@
-
 let leaderboardData = [];
 let list = document.querySelector('#leaderboard-list');
 
-
-async function getLeaderboard() {
+async function getLeaderboard(){
     try {
-
-        const response = await fetch('http://localhost:5000/leaderboard')
+        const response = await fetch('/api/leaderboard');
         const data = await response.json();
         leaderboardData = data;
-        rederData();
+        renderData();
     } catch (error) {
         console.error(error);
-        list.innerHTML = '<li style="color:tomato; font-size:.9rem;">Something went wrong. Check your Network! </li>';
+        list.innerHTML = '<li style="color:tomato; font-size:.9rem;">Something went wrong. Check your Network!</li>';
     }
 }
 
-function rederData() {
-
+function renderData(){
     if (leaderboardData.length === 0 || !leaderboardData) {
         list.innerHTML = '<li>No data</li>';
         return;
@@ -26,12 +22,11 @@ function rederData() {
     leaderboardData.forEach((data, index) => {
         const li = document.createElement('li');
         li.innerHTML = `
-            <span>${index + 1}</span>
-            <span>${data.name}</span>
-            <span>${data.score}</span>
-        `;
+      <span>${index + 1}</span>
+      <span>${data.name}</span>
+      <span>${data.score}</span>
+    `;
         list.appendChild(li);
-
     });
 }
 
